7c64d6333edf22770d003566608958a626271bba
t-sin <shinichi.tanaka45@gmail.com>
2018-08-30 14:36:06 +0900

    Implement basic event loops

--- secd.py
+++ secd.py
@@ -1,3 +1,4 @@
+#!/usr/bin/ppython
 
 def locate(e, n):
     '''Returns element of environment at index `n`'''
@@ -7,8 +8,64 @@ def rplaca(e, v):
     '''Replace specified value of environment??'''
     return None
 
+
+class Machine(object):
+    '''SECD machine: state and runner'''
+
+    def __init__(self):
+        self.s = []
+        self.e = []
+        self.c = []
+        self.d = None
+
+        self._stop = False
+        self._debug = False
+
+    def __getattr__(self, name):
+        if name == 'st':
+            return (self.s, self.e, self.c, self.d)
+        else:
+            return self.__getattribute__(name)
+
+    def __repr__(self):
+        return '''machine:
+  s = {}
+  e = {}
+  c = {}
+  d = {}
+'''.format(*self.st)
+
+    def step(self):
+        op = self.c[0]
+
+        if self._debug is True:
+            print(self)
+            print('  OP: {}'.format(op))
+
+        s, e, c, d = opcode[op[0]](self, *op[1:])
+        self.s = s
+        self.e = e
+        self.c = c
+        self.d = d
+
+    def run(self):
+        while not self._stop:
+            if len(self.c) == 0:
+                print('Machine stopped by empty code.')
+                print(self)
+                return
+
+            self.step()
+
+        print('Machine stopped.')
+        print(self)
+
+
+# instructions
+#
+# they take few args; secd machine and others...
 opcode = {
-    'ld': None,
+    'ld': lambda m, n: ([e.n] + m.s, m.e, m.c[1:], m.d),
     'ldc': None,
     'ldf': None,
     'ap': None,
@@ -31,20 +88,9 @@ opcode = {
     'stop': None,
 }
 
-class SECD(object):
-    '''SECD machine: state and runner'''
+if __name__ == '__main__':
+    code = []
+    m = Machine()
+    m._debug = True
 
-    def __init__(self):
-        self.s = []
-        self.e = []
-        self.c = []
-        self.d = None
-
-    def __init__(self, s, e, c):
-        self.s = s
-        self.e = e
-        self.c = c
-        self.d = None
-
-    def run():
-        pass
+    m.run()
