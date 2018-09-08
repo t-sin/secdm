2b40cfd1fc0c012ce2c10b1a0fe71afdd6d73122
t-sin <shinichi.tanaka45@gmail.com>
2018-09-08 19:33:55 +0900

    Program for SECE machine: counting number up

--- program/countup.s
+++ program/countup.s
@@ -1,28 +1,28 @@
-;;;; WIP: count up numbers
-;;;; oooh..???
+;;;; count up numbers
 
-;; takes `n`. if `n == 0`,
-;; then decrement `n` and recursively apply `(ld 1)`,
-;; otherwise just return.
-(ldf ((ld 0) (write)
-      (ld 0) (ldc 0) (eq)
-      (sel ((join))
-           ((ld 0) (ldc -1) (add)
-            (rap)
+;; push dummy value into current environment.
+(dum)
+
+;; push a counter on the stack top
+(ldc 0)
+
+;; defines the recursive function `rf`:
+;; this applies `rf` in the cons, when cdr of arg is equals to the limit number `10`.
+(ldf ((ld 0) (cdr) (ldc 10) (eq)
+      (sel ((ldc "stop counting up.") (write) (join))
+           ((ld 0) (cdr) (write)        ; print current counter
+            (ld 0) (cdr) (ldc 1) (add)  ; increment new number
+            (ld 0) (car)                ; take `rf` from cons cel
+            (cons)                      ; combine new number and `rf`
+            (ld 0) (car) (ap)           ; apply `rf` with new cell
             (join)))
       (rtn)))
 
+;; stores a limit number and the recursion function to a cons cell.
+(cons)
 
-;; starter part of recursion.
-;; here, number of count is set to stack top.
-(dum)
-(ldf ((ldc 10)
-      (dum)
-      (ldf ((ld 0)
-            (ld 1)
-            (rap)
-            (rtn)))
-      (rap)
+;; the starter function `sf`
+(ldf ((ld 0) (ld 0) (car) (rap)
       (rtn)))
 
 (ap)
