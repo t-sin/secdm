573c3c235acfb6a9bda88b9296f2769cda527f84
t-sin <shinichi.tanaka45@gmail.com>
2018-09-08 15:03:03 +0900

    Program for SECD machine: infinite loop

---
+++ program/loop.s
@@ -0,0 +1,24 @@
+;;;; infinite loop
+
+;; push dummy value into current environment.
+(dum)
+
+;; define the recursive function `rf`: it takes itsself, and call itsself with args; itsself.
+;; function defined is pushed into stack top.
+;; function definition copies current envrionment
+(ldf ((ldc "looping...") (write)
+      (ld 0) (ld 0) (ap)
+      (rtn)))
+
+;; starter function `sf`:
+;; new envrionment is created and it stores the `rf` because the function
+;; is argument of starter function.
+;; the starter function applies `rf` with one args `rf`.
+(ldf ((ld 0) (ld 0) (rap)
+      (rtn)))
+
+;; apply the starter function `sf`
+(ap)
+
+;; stop machine
+(stop)
