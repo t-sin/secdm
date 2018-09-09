1c445f062536ae5f680ca8629f06137bfa9c405e
t-sin <shinichi.tanaka45@gmail.com>
2018-09-09 22:34:18 +0900

    Program for SECD machine: calculate Fibonacci sequence

--- program/fib.s
+++ program/fib.s
@@ -1,11 +1,49 @@
-;; fibonacci
+;;;; calculate fibonacci number
 
-(ldc 11)
-(ldf ((ld 0)
-      (ldc 1)
-      (eq)
-      (sel ((ldc 1) (join))
-           ((ldc "hoge") (write) (join)))
+(dum)
+
+(ldc 0)   ; counter 
+(ldc 20)  ; number of max n
+(cons)
+
+;; fib-fn: (fib-fn . n) => F_n
+(ldf ((ld 0) (cdr) (ldc 0) (eq) ;; F_0
+      (sel ((ldc 0) (join))
+           ((ld 0) (cdr) (ldc 1) (eq) ;; F_1
+            (sel ((ldc 1) (join))
+                 ((ld 0) (cdr) (ldc -2) (add) (ld 0) (car) (cons) (ld 0) (car) (ap)
+                  (ld 0) (cdr) (ldc -1) (add) (ld 0) (car) (cons) (ld 0) (car) (ap)
+                  (add)
+                  (join)))
+            (join)))
       (rtn)))
+(cons)
+
+;; rec-fn: (rec-fn . (fib-fn . (max . n))) -> _
+(ldf ((ld 0) (cdr) (cdr) (cdr) (ld 0) (cdr) (cdr) (car) (eq)
+      (sel ((join))
+           (;; call fib-fn
+            (ld 0) (cdr) (cdr) (cdr)
+            (ld 0) (cdr) (car)
+            (cons)
+            (ld 0) (cdr) (car)
+            (ap) (write)
+            ;; call rec-fn
+            (ld 0) (cdr) (cdr) (cdr) (ldc 1) (add)
+            (ld 0) (cdr) (cdr) (car)
+            (cons)
+            (ld 0) (cdr) (car)
+            (cons)
+            (ld 0) (car)
+            (cons)
+            (ld 0) (car)
+            (ap)
+            (join)))
+      (rtn)))
+(cons)
+
+(ldf ((ld 0) (ld 0) (car) (rap)
+      (rtn)))
+
 (ap)
 (stop)
