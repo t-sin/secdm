c7fc4d836b24de630461ff936622a8c4cc5404e0
t-sin <shinichi.tanaka45@gmail.com>
2018-09-10 15:07:02 +0900

    Base object class

--- object.py
+++ object.py
@@ -1,14 +1,19 @@
 '''SECD machine object'''
 
+class Object(object):
+    pass
 
-class Symbol(object):
+class Nil(Object):
+    pass
+
+class Symbol(Object):
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
         return '{}'.format(self.name.upper())
 
-class Cons(object):
+class Cons(Object):
     def __init__(self, car, cdr):
         self.car = car
         self.cdr = cdr
@@ -16,7 +21,7 @@ class Cons(object):
     def __repr__(self):
         return '({} . {})'.format(self.car, self.cdr)
 
-class Func(object):
+class Func(Object):
     def __init__(self, code, env):
         self.code = code
         self.env = env
