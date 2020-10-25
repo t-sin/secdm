8b3854c9f4db01258ff9a3c57640ff66e2548704
t-sin <shinichi.tanaka45@gmail.com>
2020-10-25 17:24:58 +0900

    Compiles n-arity functon applications

--- compile.lisp
+++ compile.lisp
@@ -5,6 +5,9 @@
 (in-package #:secdm.compile)
 
 (defun replace-arg (arg n code)
+  "Replace all symbol named `arg` with `n`.
+
+  It's used to embed argument marker (see `compile-lambda`)."
   (if (null code)
       code
       (if (listp code)
@@ -21,14 +24,38 @@
   (compiling-name nil)
   (name-table (make-hash-table)))
 
-(defun compile-lambda (args body state &optional (nest 0))
-  "0-arity function is ether a constant or process that has side effect."
-  ;; maybe wip
+(defun compile-lambda (args body state &optional (argnum (1- (length args))))
+  "Compile lambda expression to SECD machine code.
+
+  Because functions in SECD machine are curried, so multiple number of arguments are
+  compiled into nested 1-arity functions. But SECD machine clears S stack when `ap` or `rap`,
+  so we cannot pass actual arguments over functional application. Here, this function
+  embeds argument marker (it is just an integer) into the place of actual argument.
+
+  Note that 0-arity function is ether a constant or process that has side effect but
+  there is no 0-arity function."
   (if (null args)
       (compile-lisp-1 body state)
       (let ((args* (cdr args))
-            (body* (replace-arg (car args) nest body)))
-        `((ldf (,@(compile-lambda args* body* state (1+ nest)) (rtn)))))))
+            (body* (replace-arg (car args) argnum body)))
+        (let ((inner (compile-lambda args* body* state (1- argnum))))
+          ;; argnum should be replaced with actual argument code when function application
+          `((ldf (,@(unless (zerop argnum) (list argnum)) ,@inner (rtn))) (ap))))))
+
+(defun compile-apply (args code state)
+  "Compile function application.
+
+  This function replaces argument marker with actual actument expression."
+  (if (null args)
+      code
+      (let* ((op (first code))
+             (op-name (first op))
+             (op-body (second op)))
+        (if (and (eq op-name 'ldf) (numberp (first op-body)))
+            `((ldf (,@(compile-lisp-1 (elt args 0) state)
+                    ,@(compile-apply (cdr args) (rest op-body) state)))
+              ,@(rest code))
+            code))))
 
 (defun compile-lisp-1 (code state)
   "Compile Lisp code to SECD machine code."
@@ -70,11 +97,12 @@
                 ;; function applications
                 (t (let ((name (first code))
                          (args (rest code)))
-                     `(,@(loop
-                           :for a :in args
-                           :append (compile-lisp-1 a state))
-                       ,@(gethash name (compiler-state-name-table state))
-                       (ap))))))))
+                     (let ((vcode (gethash name (compiler-state-name-table state))))
+                       (when (null vcode)
+                         (error "undefined name `~s`." name))
+                       (let ((fncode (compile-apply (rest args) vcode state)))
+                       `(,@(compile-lisp-1 (first args) state)
+                         ,@fncode)))))))))
     (keyword `((ld ,(parse-integer (symbol-name code)))))
     (symbol (let ((val (gethash code (compiler-state-name-table state))))
               (if val
