95b8beaf264bd20d9d0f93558f20c81e4f14e914
t-sin <shinichi.tanaka45@gmail.com>
2020-10-25 14:11:48 +0900

    Compile 1-arity function applications

--- compile.lisp
+++ compile.lisp
@@ -4,28 +4,31 @@
            #:compile-lisp))
 (in-package #:secdm.compile)
 
-(defun find-variable (v code)
+(defun replace-arg (arg n code)
   (if (null code)
-      nil
-      (let ((bound-p nil))
-        (dolist (c (rest code))
-          (setf bound-p (or bound-p
-                            (typecase c
-                              (symbol (eq v c))
-                              (list (find-variable v c))
-                              (t nil)))))
-        bound-p)))
+      code
+      (if (listp code)
+          (loop
+            :for c :in code
+            :collect (if (listp c)
+                         (replace-arg arg n c)
+                         (if (eq c arg)
+                             (intern (format nil "~a" n) :keyword)
+                             c)))
+          code)))
 
 (defstruct compiler-state
   (compiling-name nil)
   (name-table (make-hash-table)))
 
-(defun compile-lambda (args body state)
+(defun compile-lambda (args body state &optional (nest 0))
   "0-arity function is ether a constant or process that has side effect."
   ;; maybe wip
   (if (null args)
       (compile-lisp-1 body state)
-      `((ldf ,(compile-lambda (cdr args) body state)))))
+      (let ((args* (cdr args))
+            (body* (replace-arg (car args) nest body)))
+        `((ldf (,@(compile-lambda args* body* state (1+ nest)) (rtn)))))))
 
 (defun compile-lisp-1 (code state)
   "Compile Lisp code to SECD machine code."
@@ -35,25 +38,48 @@
             (let ((name (intern (symbol-name op) :keyword)))
               (case name
                 ;; 1-arity functions
-                (:atom `(,@(compile-lisp-1 (second code)) (atom)))
-                (:car `(,@(compile-lisp-1 (second code)) (car)))
-                (:cdr `(,@(compile-lisp-1 (second code)) (cdr)))
+                (:atom `(,@(compile-lisp-1 (second code) state) (atom)))
+                (:car `(,@(compile-lisp-1 (second code) state) (car)))
+                (:cdr `(,@(compile-lisp-1 (second code) state) (cdr)))
                 ;; 2-arity functions
-                (:cons `(,@(compile-lisp-1 (second code))
-                         ,@(compile-lisp-1 (third code))
+                (:cons `(,@(compile-lisp-1 (second code) state)
+                         ,@(compile-lisp-1 (third code) state)
                          (cons)))
-                (:eq `(,@(compile-lisp-1 (second code))
-                       ,@(compile-lisp-1 (third code))
+                (:eq `(,@(compile-lisp-1 (second code) state)
+                       ,@(compile-lisp-1 (third code) state)
                        (eq)))
+                ;; 2-arity arithmetic functions
                 ;; special forms
                 (:quote `((ldc ,(second code))))
                 (:if (let ((cond (second code))
                            (true (third code))
                            (false (fourth code)))
-                       `(,@(compile-lisp-1 cond)
-                         (sel (,@(compile-lisp-1 true) (join))
-                              (,@(compile-lisp-1 false) (join))))))
-                (:lambda (compile-lambda (second code) (third code)))))))
+                       `(,@(compile-lisp-1 cond state)
+                         (sel (,@(compile-lisp-1 true state) (join))
+                              (,@(compile-lisp-1 false state) (join))))))
+                (:lambda (compile-lambda (second code) (third code) state))
+                (:define (let ((name (second code))
+                               (body (third code)))
+                           (if (symbolp name)
+                               (progn
+                                 (setf (compiler-state-compiling-name state) name)
+                                 (setf (gethash name (compiler-state-name-table state))
+                                       (compile-lisp-1 body state))
+                                 (setf (compiler-state-compiling-name state) nil))
+                               (error "`~s` is not a symbol." name))))
+                ;; function applications
+                (t (let ((name (first code))
+                         (args (rest code)))
+                     `(,@(loop
+                           :for a :in args
+                           :append (compile-lisp-1 a state))
+                       ,@(gethash name (compiler-state-name-table state))
+                       (ap))))))))
+    (keyword `((ld ,(parse-integer (symbol-name code)))))
+    (symbol (let ((val (gethash code (compiler-state-name-table state))))
+              (if val
+                  val
+                  (error "undefined name: `~s`." code))))
     (t `((ldc ,code)))))
 
 (defun compile-lisp (code-list)
